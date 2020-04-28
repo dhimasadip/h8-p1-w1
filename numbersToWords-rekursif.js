9795f81d0a24c971a4c385a846c859b70e1f47b9
dhimasadip <dhimasadip.dap@gmail.com>
2020-04-28 19:13:05 +0700

    coba

---
+++ numbersToWords-rekursif.js
@@ -0,0 +1,40 @@
+function numberToWords(number) {
+    // Your code here
+    const dict = [
+      [0, ''],
+      [1, 'satu'],
+      [2, 'dua'],
+      [3, 'tiga'],
+      [4, 'empat'],
+      [5, 'lima'],
+      [6, 'enam'],
+      [7, 'tujuh'],
+      [8, 'delapan'],
+      [9, 'sembilan'],
+      [10, 'sepuluh'],
+      [11, 'sebelas'],
+      [12, 'dua belas'],
+      [13, 'tiga belas'],
+      [14, 'empat belas'],
+      [15, 'lima belas'],
+      [16, 'enam belas'],
+      [17, 'tujuh belas'],
+      [18, 'delapan belas'],
+      [19, 'sembilan belas'],
+      [100, 'seratus'],
+      [1000, 'seribu']
+    ]
+
+  
+  
+}
+  
+  
+  
+  
+  // Driver code
+  console.log(numberToWords(705));
+// Tujuh Ratus Lima  
+  console.log(numberToWords(10000001));
+  console.log(numberToWords(2011845));
+  console.log(numberToWords(2453321845));
